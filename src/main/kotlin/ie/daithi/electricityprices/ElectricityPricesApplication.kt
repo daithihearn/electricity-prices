6f3134e487b932dc4608f749704bdc997f2b8445
Daithi Hearn <daithi.hearn@gmail.com>
2023-04-15 13:31:45 +0200

    Updating dependencies

--- src/main/kotlin/ie/daithi/electricityprices/ElectricityPricesApplication.kt
+++ src/main/kotlin/ie/daithi/electricityprices/ElectricityPricesApplication.kt
@@ -1,9 +1,23 @@
 package ie.daithi.electricityprices
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@OpenAPIDefinition(
+	security = [SecurityRequirement(name = "bearerAuth")],
+	info = Info(
+		title =
+		"Electricity Prices API",
+		version = "1.0.0",
+		description = "Returns PVPC electricity prices for a given range"
+	)
+)
+@SpringBootApplication(scanBasePackages = ["ie.daithi.electricityprices"])
 class ElectricityPricesApplication
 
 fun main(args: Array<String>) {
