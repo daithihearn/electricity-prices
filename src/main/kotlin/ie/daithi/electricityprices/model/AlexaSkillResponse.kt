e1d7368c84bd43e7da8b77e331a7dd2039d4bb73
Daithi Hearn <daithi.hearn@gmail.com>
2023-07-22 22:43:31 +0200

    feat: adding alexa skill endpoint

--- src/main/kotlin/ie/daithi/electricityprices/model/AlexaSkillResponse.kt
+++ src/main/kotlin/ie/daithi/electricityprices/model/AlexaSkillResponse.kt
@@ -1,3 +1,11 @@
 package ie.daithi.electricityprices.model
 
-data class AlexaSkillResponse()
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class AlexaSkillResponse(
+    val uid: String = UUID.randomUUID().toString(),
+    val updateDate: String = LocalDateTime.now().toString(),
+    val titleText: String,
+    val mainText: String
+)
--- src/main/kotlin/ie/daithi/electricityprices/web/controller/AlexaSkillController.kt
+++ src/main/kotlin/ie/daithi/electricityprices/web/controller/AlexaSkillController.kt
@@ -1,4 +1,37 @@
 package ie.daithi.electricityprices.web.controller
 
-class AlexaSkillController {
+import ie.daithi.electricityprices.model.AlexaSkillResponse
+import ie.daithi.electricityprices.service.PriceService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "Alexa", description = "Endpoints that relate to the electricity prices alexa skill")
+class AlexaSkillController(private val priceSerice: PriceService) {
+    @GetMapping("/alexa")
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(summary = "Get info for the alexa skill", description = "Returns the JSON message feed for the electricity prices alexa skill")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Request successful")
+    )
+    @ResponseBody
+    fun getFeed (): AlexaSkillResponse {
+        val now = LocalDateTime.now()
+        val prices = priceSerice.getPrices(now.toLocalDate())
+
+        // Get current price
+        val currentPrice = prices.find { it.dateTime.hour == now.hour }
+
+        return AlexaSkillResponse(
+            updateDate = now.toString(),
+            titleText = "Electricity Prices",
+            mainText = "The current price is ${currentPrice?.price} cents per kWh"
+        )
+    }
 }
