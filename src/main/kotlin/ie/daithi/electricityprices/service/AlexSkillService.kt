05d551d2600c4ba6b2cd9d83cfb0144a465313a7
Daithi Hearn <daithi.hearn@gmail.com>
2023-07-23 12:40:57 +0200

    feat: adding next good period message

--- src/main/kotlin/ie/daithi/electricityprices/service/AlexSkillService.kt
+++ src/main/kotlin/ie/daithi/electricityprices/service/AlexSkillService.kt
@@ -4,6 +4,7 @@ import ie.daithi.electricityprices.model.AlexaSkillResponse
 import ie.daithi.electricityprices.model.Price
 import ie.daithi.electricityprices.utils.alexaSkillFormatter
 import ie.daithi.electricityprices.utils.dateFormatter
+import ie.daithi.electricityprices.utils.getCheapestPeriod
 import org.apache.logging.log4j.LogManager
 import org.springframework.context.MessageSource
 import org.springframework.stereotype.Service
@@ -15,6 +16,7 @@ import kotlin.math.roundToInt
 class AlexSkillService(private val priceSerice: PriceService, private val messageSource: MessageSource) {
 
     private val logger = LogManager.getLogger(this::class.simpleName)
+
     fun getResponses(locale: Locale): List<AlexaSkillResponse> {
         val responses = mutableListOf<AlexaSkillResponse>()
 
@@ -32,16 +34,49 @@ class AlexSkillService(private val priceSerice: PriceService, private val messag
         // Today Rating
         responses.add(getDayRating(pricesToday, now, locale))
 
+        // Get next good 3-hour period
+        val nextCheapPeriod = getNextCheapPeriod(pricesToday, now, locale)
+        nextCheapPeriod?.let { responses.add(it) }
+
+
         // Get Tomorrow's price data
         val tomorrow = now.plusDays(1)
         val pricesTomorrow = priceSerice.getPrices(tomorrow.toLocalDate())
 
-        if (!pricesTomorrow.isEmpty()) {
+        if (pricesTomorrow.isNotEmpty()) {
             // Tomorrow Rating
             responses.add(getDayRating(pricesTomorrow, tomorrow, locale))
         }
 
         return responses
+    }
+
+    private fun getNextCheapPeriod(pricesToday: List<Price>, now: LocalDateTime, locale: Locale): AlexaSkillResponse? {
+        val cheapestPeriod = getCheapestPeriod(pricesToday, 3)
+
+        // If the period has passed do nothing
+        if (cheapestPeriod[2].dateTime.plusMinutes(59).isBefore(now)) {
+            return null
+        }
+
+        // Get average price for period
+        val averagePrice = cheapestPeriod.map { it.price }.average()
+
+        // If period hasn't started send message
+        if (cheapestPeriod[0].dateTime.isAfter(now)) {
+            return AlexaSkillResponse(
+                updateDate = now.format(alexaSkillFormatter),
+                titleText = messageSource.getMessage("alexa.next.cheap.period.title", emptyArray(), locale),
+                mainText = messageSource.getMessage("alexa.next.cheap.period.main", arrayOf(cheapestPeriod[0].dateTime.hour, averagePrice), locale)
+            )
+        } else {
+            // We are currently in the good period
+            return AlexaSkillResponse(
+                updateDate = now.format(alexaSkillFormatter),
+                titleText = messageSource.getMessage("alexa.current.cheap.period.title", emptyArray(), locale),
+                mainText = messageSource.getMessage("alexa.current.cheap.period.main", arrayOf(cheapestPeriod[0].dateTime.hour, averagePrice), locale)
+            )
+        }
     }
 
     /**
@@ -70,7 +105,7 @@ class AlexSkillService(private val priceSerice: PriceService, private val messag
         // If it's less than 2 cents then it's a bad day
         val thirtyDayAverage = priceSerice.getPrices(start = dateTime.toLocalDate().minusDays(30).format(dateFormatter), end = null).map { it.price }.average()
 
-        val roundedDailyAverage = arrayOf(dailyAverage.times(100)?.roundToInt())
+        val roundedDailyAverage = dailyAverage.times(100).roundToInt()
 
         // Check if is today or tomorrow
         val day = if (dateTime.toLocalDate() == LocalDateTime.now().toLocalDate()) "today" else "tomorrow"
