264a1136e4a5dabde3c23239cae7deb319657200
Daithi Hearn <daithi.hearn@gmail.com>
2023-07-25 23:59:08 +0200

    fix: resolving merge

--- src/main/kotlin/ie/daithi/electricityprices/web/controller/AlexaSkillController.kt
+++ src/main/kotlin/ie/daithi/electricityprices/web/controller/AlexaSkillController.kt
@@ -17,14 +17,77 @@ class AlexaSkillController(private val alexSkillService: AlexSkillService) {
 
     @GetMapping("/alexa")
     @ResponseStatus(value = HttpStatus.OK)
-    @Operation(summary = "Get info for the alexa skill", description = "Returns the JSON message feed for the electricity prices alexa skill")
+    @Operation(
+        summary = "Get info for the alexa skill",
+        description = "Returns the JSON message feed for the electricity prices alexa skill"
+    )
     @ApiResponses(
         ApiResponse(responseCode = "200", description = "Request successful")
     )
     @ResponseBody
-    fun getFeed(@RequestParam(value = "locale", required = false) locale: String?): List<AlexaSkillResponse> {
+    fun getFullFeed(@RequestParam(value = "locale", required = false) locale: String?): List<AlexaSkillResponse> {
         val resolvedLocale = locale?.let { Locale.forLanguageTag(it) } ?: Locale.forLanguageTag("es")
-        return alexSkillService.getResponses(resolvedLocale)
+        return alexSkillService.getFullFeed(resolvedLocale)
+    }
+
+    @GetMapping("/alexa/today")
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+        summary = "Get today's price and rating",
+        description = "Returns the JSON message for today's price and rating"
+    )
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Request successful")
+    )
+    @ResponseBody
+    fun getTodayRating(@RequestParam(value = "locale", required = false) locale: String?): AlexaSkillResponse {
+        val resolvedLocale = locale?.let { Locale.forLanguageTag(it) } ?: Locale.forLanguageTag("es")
+        return alexSkillService.getTodayRating(locale = resolvedLocale)
+    }
+
+    @GetMapping("/alexa/tomorrow")
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+        summary = "Get tomorrow's price and rating",
+        description = "Returns the JSON message for tomorrow's price and rating"
+    )
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Request successful")
+    )
+    @ResponseBody
+    fun getTomorrowRating(@RequestParam(value = "locale", required = false) locale: String?): AlexaSkillResponse {
+        val resolvedLocale = locale?.let { Locale.forLanguageTag(it) } ?: Locale.forLanguageTag("es")
+        return alexSkillService.getTomorrowRating(locale = resolvedLocale).first
+    }
+
+    @GetMapping("/alexa/cheap/next")
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+        summary = "Get the next cheap period",
+        description = "Returns the JSON message for the next cheap period"
+    )
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Request successful")
+    )
+    @ResponseBody
+    fun getNextCheapPeriod(@RequestParam(value = "locale", required = false) locale: String?): AlexaSkillResponse {
+        val resolvedLocale = locale?.let { Locale.forLanguageTag(it) } ?: Locale.forLanguageTag("es")
+        return alexSkillService.getNextCheapPeriod(locale = resolvedLocale).first
+    }
+
+    @GetMapping("/alexa/expensive/next")
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(
+        summary = "Get the next expensive period",
+        description = "Returns the JSON message for the next expensive period"
+    )
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Request successful")
+    )
+    @ResponseBody
+    fun getNextExpensivePeriod(@RequestParam(value = "locale", required = false) locale: String?): AlexaSkillResponse {
+        val resolvedLocale = locale?.let { Locale.forLanguageTag(it) } ?: Locale.forLanguageTag("es")
+        return alexSkillService.getNextExpensivePeriod(locale = resolvedLocale).first
     }
 
 }
