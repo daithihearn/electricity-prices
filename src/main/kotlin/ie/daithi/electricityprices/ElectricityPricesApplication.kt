d3db79f0917df76fa5c34aa0b1bbb966a7df84dd
Daithi Hearn <daithi.hearn@gmail.com>
2023-07-29 21:29:34 +0200

    fix: updating swagger version

--- src/main/kotlin/ie/daithi/electricityprices/ElectricityPricesApplication.kt
+++ src/main/kotlin/ie/daithi/electricityprices/ElectricityPricesApplication.kt
@@ -7,19 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @OpenAPIDefinition(
-	info = Info(
-		title =
-		"Electricity Prices API",
-		version = "1.1.4",
-		description = "Returns PVPC electricity prices for a given range"
-	),
-	servers = [
-		Server(url = "/", description = "Electricity Prices API"),
-	]
+    info = Info(
+        title =
+        "Electricity Prices API",
+        version = "1.8.5",
+        description = "Returns PVPC electricity prices for a given range"
+    ),
+    servers = [
+        Server(url = "/", description = "Electricity Prices API"),
+    ]
 )
 @SpringBootApplication(scanBasePackages = ["ie.daithi.electricityprices"])
 class ElectricityPricesApplication
 
 fun main(args: Array<String>) {
-	runApplication<ElectricityPricesApplication>(*args)
+    runApplication<ElectricityPricesApplication>(*args)
 }
